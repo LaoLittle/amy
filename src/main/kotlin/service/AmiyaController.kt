52012744ddea3f8719b8d4d325f96956c4a7cd06
LaoLittle <y1941803875@outlook.com>
2021-12-25 20:49:25 +0800

    update

--- src/main/kotlin/service/AmiyaController.kt
+++ src/main/kotlin/service/AmiyaController.kt
@@ -3,7 +3,6 @@ package org.laolittle.plugin.service
 import net.mamoe.mirai.event.GlobalEventChannel
 import net.mamoe.mirai.event.Listener
 import net.mamoe.mirai.event.events.GroupMessageEvent
-import net.mamoe.mirai.event.events.MessageEvent
 import org.laolittle.plugin.AmiyaFunction
 import org.laolittle.plugin.AmiyaFunction.SIGN_IN
 
@@ -11,6 +10,4 @@ object AmiyaController {
     fun enableAmiya(type: AmiyaFunction): Listener<*> = when (type) {
         SIGN_IN -> GlobalEventChannel.subscribeAlways<GroupMessageEvent> { }
     }
-
-    fun disableAmiya(): Listener<MessageEvent> = GlobalEventChannel.subscribeAlways { }
 }
