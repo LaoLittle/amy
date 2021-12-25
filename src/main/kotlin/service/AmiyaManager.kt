0b5931e823cf46cc3dac59c32de1ebb2d4c2d5c8
LaoLittle <y1941803875@outlook.com>
2021-12-25 18:49:01 +0800

    update

--- src/main/kotlin/service/AmiyaManager.kt
+++ src/main/kotlin/service/AmiyaManager.kt
@@ -1,20 +1,38 @@
 package org.laolittle.plugin.service
 
+import net.mamoe.mirai.console.permission.PermissionService.Companion.hasPermission
+import net.mamoe.mirai.console.permission.PermitteeId.Companion.permitteeId
+import net.mamoe.mirai.contact.isOperator
 import net.mamoe.mirai.event.GlobalEventChannel
-import net.mamoe.mirai.event.subscribeGroupMessages
+import net.mamoe.mirai.event.events.GroupMessageEvent
+import net.mamoe.mirai.message.data.content
+import net.mamoe.mirai.utils.info
 import org.laolittle.plugin.AmiyaBot
 import org.laolittle.plugin.AmiyaBot.registerPermission
 import org.laolittle.plugin.Service
 
 object AmiyaManager : Service() {
+    private val logger = AmiyaBot.logger
     override suspend fun main() {
-        val amiyaPerm = AmiyaBot.registerPermission(
+        val amiyaAdministratorPerm = AmiyaBot.registerPermission(
             "amiya.operator",
             "Amiya-Bot 管理员权限"
         )
-        GlobalEventChannel.subscribeGroupMessages {
-            finding(Regex("(?i)(阿米娅|amiya)(休息|下班)")){
-                TODO("暂时做到这")
+        GlobalEventChannel.subscribeAlways<GroupMessageEvent> {
+            val matchResult = Regex("(?i)(?:阿米娅|amiya)(.*)").find(message.content)
+            if ((sender.isOperator() || sender.permitteeId.hasPermission(amiyaAdministratorPerm)) && (matchResult != null)){
+                logger.info { matchResult.groupValues.toString() }
+                when (matchResult.groupValues[1]) {
+                    "上班" -> {
+                        TODO()
+                    }
+                    "下班" -> {
+                        TODO()
+                    }
+                    "功能关闭列表" -> {
+                        TODO()
+                    }
+                }
             }
         }
     }
