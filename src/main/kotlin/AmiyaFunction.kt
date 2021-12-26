06e04543cced30af0a167d0995bef523629764b7
LaoLittle <y1941803875@outlook.com>
2021-12-27 00:02:24 +0800

    add more

--- src/main/kotlin/AmiyaFunction.kt
+++ src/main/kotlin/AmiyaFunction.kt
@@ -4,10 +4,35 @@ enum class AmiyaFunction {
     NUDGE {
         override fun toString(): String = "戳一戳"
     },
-    RESPONSE{
+    RESPONSE {
         override fun toString(): String = "普通回应"
     },
     SIGN_IN {
         override fun toString(): String = "签到"
     },
+    GACHA_SIMULATE {
+        override fun toString(): String = "模拟抽卡"
+    },
+    QUERY_OPERATOR {
+        override fun toString(): String = "干员查询"
+    },
+    QUERY_ENEMY {
+        override fun toString(): String = "敌人查询"
+    },
+    QUERY_ITEM {
+        override fun toString(): String = "物品查询"
+    },
+    QUERY_OFFER {
+        override fun toString(): String = "公招查询"
+    },
+    CALC_GET {
+        override fun toString(): String = "合成玉计算"
+    },
+    WEIBO_POST {
+        override fun toString(): String = "微博推送"
+    },
+    SEE_SOURCECODE {
+        override fun toString(): String = "查看阿米娅的源代码"
+    }
+
 }
