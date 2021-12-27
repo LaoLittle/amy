fc6c08aea1a75de8c327ed801b228e705192b41c
LaoLittle <y1941803875@outlook.com>
2021-12-27 16:35:31 +0800

    use alibaba druid as pool

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,16 +11,18 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     maven("https://maven.aliyun.com/repository/public")
+    mavenCentral()
 }
 
 dependencies {
     val exposedVersion = "0.36.2"
     val ktorVersion = "1.6.7"
-    val quartzVersion = "2.3.2"
+    implementation("org.xerial:sqlite-jdbc:3.36.0.2")
+    implementation("com.alibaba:druid:1.2.8")
     implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
     implementation("io.ktor:ktor-client-core-jvm:$ktorVersion")
     implementation("io.ktor:ktor-client-okhttp:$ktorVersion")
-    implementation("org.quartz-scheduler:quartz:$quartzVersion")
+    implementation("org.quartz-scheduler:quartz:2.3.2")
 }
